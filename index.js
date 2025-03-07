da26f8661d97e8bced3ad136fcb23f2ee2acb64b
João Pedro <141282511+jpp000@users.noreply.github.com>
2025-03-07 17:33:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World", status: 200 });
 });
 
+app.get("/new", (req, res) => {
+  res.send({ message: "New Route", status: 200 });
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
