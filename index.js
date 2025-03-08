1e1f5d4929bb6afb59247430ddb66599f516f556
João Pedro <albuquerquejoaopedro04@gmail.com>
2025-03-08 15:24:54 -0300

    add users route

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World", status: 200 });
 });
 
-app.get("/new", (req, res) => {
-  res.send({ message: "New Route", status: 200 });
+app.get("/users", (req, res) => {
+  res.send({ message: "users route", status: 200 });
 })
 
 app.listen(3000, () => {
